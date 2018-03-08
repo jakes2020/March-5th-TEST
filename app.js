a8618de142b4412af95d39035b124a0602eea1ab
jakes2020 <19jacris@waipahuh.k12.hi.us>
2018-03-07 14:40:34 -1000

    A+ worthy

---
+++ app.js
@@ -0,0 +1,23 @@
+var list = [
+"Brownie",
+"Cookie",
+"Donut",
+"Icecream"
+];
+
+var Vegetable;
+
+list.pop();
+
+list.push("Vegetable");
+
+for (i = 0; i < list.length; i++) {
+	
+	list.pop();
+	list.push("Vegetable");
+};
+
+console.log("list")
+
+document.getElementById("article").innerhtml = 
+
